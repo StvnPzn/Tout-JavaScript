97279c8dbaad32c8970901ac1d432c2b9c26d3c4
Steven Pouzon <spouzon@gmail.com>
2021-07-19 16:00:43 +0200

    starting script chapter

---
+++ Script basics/hello.js
@@ -0,0 +1,11 @@
+alert("Hello Worl via un fichier JS externe");
+
+var a;
+var n1 = 10;
+var msg = "Hello World";
+var h1 = document.getElementById("monH1");
+
+console.log(typeof a);
+console.log(typeof n1);
+console.log(typeof msg);
+console.log(typeof h1);
